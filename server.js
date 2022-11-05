b110608939994332dd3379ec2cc4e2bd9ed076d6
britneyfrizzell@gmail.com <95828527+bfrizzell93@users.noreply.github.com>
2022-11-05 12:16:23 -0500

    routers

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express'); //since its in nodemodules, doesnt have to have file path
-const morgan = require('morgan');
-const campsiteRouter = require('./routes/campsiteRouter');
+const express = require("express"); //since its in nodemodules, doesnt have to have file path
+const morgan = require("morgan");
+const campsiteRouter = require("./routes/campsiteRouter");
+const partnerRouter = require("./routes/partnerRouter");
+const promotionRouter = require("./routes/promotionRouter");
 
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
 
 const app = express(); //returns express server app
-app.use(morgan('dev')); //logs using development version
+app.use(morgan("dev")); //logs using development version
 app.use(express.json()); //parses json to JS for use
 
-app.use('/campsites', campsiteRouter)
+app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
 
-
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
 app.use((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>This is an Express Server</h1></body></html>");
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
-
